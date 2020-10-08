030009488d2c287f402eda72124702451cb183bd
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-08 17:28:31 +0530

    Refactored : Added Test Cases to check the size of a Given Tree

--- src/main/java/com/capgemini/binarytree/BinaryTree.java
+++ src/main/java/com/capgemini/binarytree/BinaryTree.java
@@ -2,28 +2,30 @@ package com.capgemini.binarytree;
 
 public class BinaryTree<T extends Comparable<T>> {
 	private MyBinaryNode<T> root;
+
 	public void add(T i) {
-		this.root=this.addRecursively(root,i);
+		this.root = this.addRecursively(root, i);
 	}
+
 	private MyBinaryNode<T> addRecursively(MyBinaryNode<T> curr, T i) {
-		if(curr==null)
+		if (curr == null)
 			return new MyBinaryNode<>(i);
-		int result=i.compareTo(curr.key);
-		if(result==0)
+		int result = i.compareTo(curr.key);
+		if (result == 0)
 			return curr;
-		if(result<0)
-			curr.left=addRecursively(curr.left, i);
+		if (result < 0)
+			curr.left = addRecursively(curr.left, i);
 		else
-			curr.right=addRecursively(curr.right, i);
+			curr.right = addRecursively(curr.right, i);
 		return curr;
 	}
-	
+
 	public int getSize() {
 		return this.getSizeRecursively(root);
 	}
+
 	private int getSizeRecursively(MyBinaryNode<T> curr) {
-		return 	(curr==null)?0:1+this.getSizeRecursively(curr.left)+this.getSizeRecursively(curr.right);
+		return (curr == null) ? 0 : 1 + this.getSizeRecursively(curr.left) + this.getSizeRecursively(curr.right);
 	}
-	
 
 }
--- src/test/java/com/capgemini/binarytree/MyBinaryTreeTest.java
+++ src/test/java/com/capgemini/binarytree/MyBinaryTreeTest.java
@@ -8,13 +8,26 @@ public class MyBinaryTreeTest {
 
 	@Test
 	public void Given3Numbers_WhenAddedToTheTree_ShouldReturnSize3() {
-	BinaryTree<Integer> myBinaryTree=new BinaryTree<>();
-	myBinaryTree.add(56);
-	myBinaryTree.add(30);
-	myBinaryTree.add(70);
-	int size=myBinaryTree.getSize();
-	assertEquals(3,size);
-	
+		BinaryTree<Integer> myBinaryTree = new BinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		assertEquals(3, size);
+
+	}
+
+	@Test
+	public void GivenNumbersArray_WhenAddedToTheTree_ShouldReturnSize3() {
+		BinaryTree<Integer> myBinaryTree = new BinaryTree<>();
+		Integer[] nums = { 56, 30, 70, 22, 40, 60, 95, 11, 65, 3, 16, 63, 67 };
+		int count = 0;
+		for (Integer i : nums) {
+			myBinaryTree.add(i);
+			count++;
+		}
+		int size = myBinaryTree.getSize();
+		assertEquals(count, size);
 	}
 
 }
